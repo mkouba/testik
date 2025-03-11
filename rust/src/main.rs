3eff863c498d9dbf21cdb8ef0497ce14bf0198cd
Martin Kouba <mkouba@redhat.com>
2025-03-11 14:22:04 +0100

    Ruest example - test pro odecitani

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -6,16 +6,20 @@ fn main() {
 }
 
 fn add(val1: i32, val2: i32) -> i32 {
-    // TODO
-    0
+    val1 + val2
 }
 
 fn subtract(val1: i32, val2: i32) -> i32 {
-    // TODO
-    0
+    val1 - val2
 } 
 
 #[test]
 fn add_works() {
     assert_eq!(add(2, 2), 4);
+}
+
+
+#[test]
+fn subtract_works() {
+    assert_eq!(subtract(2, 2), 0);
 }
