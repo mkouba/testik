1ef879fa3726fb0681e3150a2669d454bb813f7f
Martin Kouba <mkouba@redhat.com>
2025-03-12 14:57:15 +0100

    Rust example - nasobeni a deleni?

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,6 +3,10 @@ fn main() {
     println!("10 + 2 = {res1}");
     let res2 = subtract(10,2);
     println!("10 - 2 = {res2}");
+    let res3 = multiply(10,2);
+    println!("10 . 2 = {res3}");
+    let res4 = divide(10,2);
+    println!("10 / 2 = {res4}");
 }
 
 fn add(val1: i32, val2: i32) -> i32 {
@@ -11,7 +15,15 @@ fn add(val1: i32, val2: i32) -> i32 {
 
 fn subtract(val1: i32, val2: i32) -> i32 {
     val1 - val2
-} 
+}
+
+fn multiply(val1: i32, val2: i32) -> i32 {
+    val1 * val2
+}
+
+fn divide(val1: i32, val2: i32) -> i32 {
+    val1 % val2
+}
 
 #[test]
 fn add_works() {
